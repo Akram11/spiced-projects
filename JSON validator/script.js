2c889701d55cac4a30a6a928b74f6ea337660a24
akram11 <akram.f11@gmail.com>
2020-08-12 23:59:37 +0200

    fixed a bug

--- JSON validator/script.js
+++ JSON validator/script.js
@@ -3,6 +3,7 @@ $("#validator").on("click", function () {
         JSON.parse($("textarea").val());
     } catch (e) {
         alert("not a valid JSON string");
+        return;
     }
     alert("Valid JSON string");
 });
