eb508e7a22251476f72864d5482cd759bd0405e7
akram11 <akram.f11@gmail.com>
2020-08-13 11:01:48 +0200

    first ajax request

---
+++ AJAX/script.js
@@ -0,0 +1,22 @@
+(function () {
+    $.ajax({
+        url: "/data.json",
+        method: "GET",
+        success: function (response) {
+            //console.log('response: ', response);
+
+            var myHtml = "";
+            for (var i = 0; i < response.length; i++) {
+                // console.log(response[i]);
+
+                var city = "<div>" + response[i].city + "</div>";
+                myHtml += city;
+            }
+
+            $("#results").html(myHtml);
+        },
+        error: function (err) {
+            console.log("err", err);
+        },
+    });
+})();
