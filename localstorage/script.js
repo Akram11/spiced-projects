1b551e5efc4ac5fbd3673efeb255df3f92ce168d
akram11 <akram.f11@gmail.com>
2020-08-13 10:54:23 +0200

    added try-catch

--- localstorage/script.js
+++ localstorage/script.js
@@ -1,7 +1,25 @@
-$("textarea").on("input", function (e) {
-    localStorage.setItem("text", $("textarea").val());
-});
+// (function () {
+//     $("textarea").on("input", function (e) {
+//         localStorage.setItem("text", $("textarea").val());
+//     });
 
-$(window).on("load", function () {
-    $("textarea").val(localStorage.getItem("text"));
-});
+//     $(window).on("load", function () {
+//         $("textarea").val(localStorage.getItem("text"));
+//     });
+// })();
+
+(function () {
+    try {
+        $("textarea").val(localStorage.getItem("storedText"));
+    } catch (e) {
+        return;
+    }
+
+    $("textarea").on("input", function (e) {
+        try {
+            localStorage.setItem("storedText", $("textarea").val());
+        } catch (e) {
+            return;
+        }
+    });
+})();
