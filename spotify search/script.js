94427170e66747fe545716e1c6d71ca12f1c1c0b
akram11 <akram.f11@gmail.com>
2020-08-17 18:41:46 +0200

    fixed a bug images url

--- spotify search/script.js
+++ spotify search/script.js
@@ -1,7 +1,9 @@
 (function () {
     var submitButton = $(".submit-button");
+    var showButton = $(".show-more");
     var nextUrl;
 
+    function ajaxCall(url, input, choice) {}
     submitButton.on("click", function () {
         var userInput = $("input[name='search']").val();
         var artistOrAlbum = $(".artist-or-album").val();
@@ -14,7 +16,6 @@
             success: function (payload) {
                 payload = payload.artists || payload.albums;
                 console.log(payload);
-
                 if (payload.next) {
                     nextUrl = payload.next.replace(
                         "https://api.spotify.com/v1/search",
@@ -29,12 +30,23 @@
                             : `NO Ruslts for: ${userInput}`
                     }`
                 );
-                var names = "";
+                var resultsCards = "";
                 for (var i = 0; i < payload.items.length; i++) {
-                    names += "<p>" + payload.items[i].name + "</p>";
-                } // closes for loop
+                    console.log(payload.items[i].images);
+                    resultsCards += `<a class="artist-card" href="${
+                        payload.items[i].external_urls.spotify
+                    }"><img class="artist-img" src="${
+                        payload.items[i].images.length == 0
+                            ? "https://developer.spotify.com/assets/branding-guidelines/icon3@2x.png"
+                            : payload.items[i].images[0].url
+                    }">
+                    <p>  ${payload.items[i].name}  </p> </a>`;
+                }
+                if (payload.total > 20) {
+                    showButton.addClass("show");
+                }
 
-                $(".results-container").html(names);
+                $(".results-container").html(resultsCards);
             },
         });
     });
