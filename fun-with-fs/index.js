dc413624f4d26f0b39bd05e9e683a95bbc234499
akram11 <akram.f11@gmail.com>
2020-08-23 23:25:34 +0200

    updtate

--- fun-with-fs/index.js
+++ fun-with-fs/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const fullPath = __dirname;
-
+console.log(fullPath);
 const logSizes = (dir) => {
     fs.readdir(dir, { withFileTypes: true }, (err, files) => {
         if (err) {
@@ -24,3 +24,34 @@ const logSizes = (dir) => {
 };
 
 logSizes(fullPath);
+
+const mapSizes = (dir) => {
+    let jsonObj = {};
+    let fileObjs = fs.readdirSync(dir, { withFileTypes: true }, (err) => {
+        console.log(err);
+    });
+
+    fileObjs.forEach((item) => {
+        let size = fs.statSync(dir + "/" + item.name, (err) => console.log(err))
+            .size;
+        if (item.isFile()) {
+            jsonObj[item.name] = size;
+        } else if (item.isDirectory()) {
+            jsonObj = {
+                ...jsonObj,
+                [item.name]: mapSizes(dir + "/" + item.name),
+            };
+        }
+    });
+
+    return jsonObj;
+};
+
+const strObj = JSON.stringify(mapSizes(fullPath), 4);
+fs.writeFile("./files.json", strObj, function (err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
+console.log(strObj);
