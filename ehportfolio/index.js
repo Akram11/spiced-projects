3e822ffc33fee9dab39e6581c297e7a8dcccd942
akram11 <akram.f11@gmail.com>
2020-08-30 19:03:47 +0200

    added desc view, nav partial, and simple styling

--- ehportfolio/index.js
+++ ehportfolio/index.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
+const projects = require("./projects.json");
 
 app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
 
+app.use(express.static("./projects"));
+app.use(express.static("./public"));
+
 app.get("/", (req, res) => {
-    res.render("hello", {
-        name: "Akram",
+    res.render("welcome", {
+        projects,
+    });
+});
+
+app.get("/projects/:project", (req, res) => {
+    const { project } = req.params;
+    const selectedProject = projects.find((p) => p.path === "/" + project);
+    if (!selectedProject) {
+        return res.sendStatus(404);
+    }
+    res.render("description", {
+        selectedProject,
+        projects,
     });
 });
 
