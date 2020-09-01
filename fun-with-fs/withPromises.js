c64da2051648d776ebe0e5357401d697c07d6d79
akram11 <akram.f11@gmail.com>
2020-09-02 00:22:34 +0200

    converted logSizes to use promises

--- fun-with-fs/withPromises.js
+++ fun-with-fs/withPromises.js
@@ -2,16 +2,22 @@ const { readdir, stat } = require("fs").promises;
 const fullPath = __dirname;
 
 const logSizes = (dir) => {
-    readdir(dir, { withFileTypes: true }).then((files) => {
-        files.forEach((item) => {
-            stat(dir + "/" + item.name).then((file) => {
-                file.isFile()
-                    ? console.log(dir + "/" + item.name + " : " + file.size)
-                    : console.log(dir + "/" + item.name + " : " + file.size);
-                // logSizes(dir + "/" + item.name);
+    readdir(dir, { withFileTypes: true })
+        .then((files) => {
+            files.forEach((item) => {
+                stat(dir + "/" + item.name).then((file) => {
+                    if (file.isFile()) {
+                        console.log(dir + "/" + item.name + " : " + file.size);
+                    } else if (file.isDirectory()) {
+                        console.log(dir + "/" + item.name + " : " + file.size);
+                        logSizes(dir + "/" + item.name);
+                    }
+                });
             });
+        })
+        .catch((err) => {
+            console.log("error in LogSizes", err);
         });
-    });
 };
 
 logSizes(fullPath);
