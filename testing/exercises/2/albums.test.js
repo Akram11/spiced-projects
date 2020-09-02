c6c9e70e7679825c3f5482ece76ebe37c2b28bed
akram11 <akram.f11@gmail.com>
2020-09-02 18:21:09 +0200

    wrote a test for getAlbumsNames

--- testing/exercises/2/albums.test.js
+++ testing/exercises/2/albums.test.js
@@ -1,12 +1,25 @@
-const {getAlbumNames} = require('./albums');
-const spotify = require('./spotify');
+const { getAlbumNames } = require("./albums");
+const spotify = require("./spotify");
 
-jest.mock('./spotify')
+jest.mock("./spotify");
 
-test('album names are in alphabetical order', () => {
-    return getAlbumNames('meat loaf').then(
-        albumNames => {
-            expect(albumNames).toEqual(albumNames.sort());
-        }
-    );
+test("album names are in alphabetical order", () => {
+    spotify.search.mockResolvedValue({
+        albums: {
+            items: [
+                {
+                    name: "a",
+                },
+                {
+                    name: "z",
+                },
+                {
+                    name: "c",
+                },
+            ],
+        },
+    });
+    return getAlbumNames("meat loaf").then((albumNames) => {
+        expect(albumNames).toEqual(albumNames.sort());
+    });
 });
