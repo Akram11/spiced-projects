919bdf84ebfbd70dc78651d6adc71d2779b38980
akram11 <akram.f11@gmail.com>
2020-09-02 16:40:43 +0200

    wrote the first two tests

--- testing/exercises/1/countries.test.js
+++ testing/exercises/1/countries.test.js
@@ -1,1 +1,12 @@
-const countries = require('./countries');
+const countries = require("./countries");
+
+test("When passed an empty string, it returns an empty array", () => {
+    expect(countries.find("")).toEqual([]);
+    expect(countries.find(" ")).toEqual([]);
+});
+
+test("The array that it returns contains no more than four matches", () => {
+    expect(countries.find("a").length).toBe(4);
+    expect(countries.find("Z").length).toBe(4);
+});
+// console.log(countries.find(" "));
