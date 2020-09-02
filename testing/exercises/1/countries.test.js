997ee116e4e4ad0be5b60973d7af309fc1567d99
akram11 <akram.f11@gmail.com>
2020-09-02 17:22:42 +0200

    wrote tests for countries.find

--- testing/exercises/1/countries.test.js
+++ testing/exercises/1/countries.test.js
@@ -6,7 +6,16 @@ test("When passed an empty string, it returns an empty array", () => {
 });
 
 test("The array that it returns contains no more than four matches", () => {
-    expect(countries.find("a").length).toBe(4);
-    expect(countries.find("Z").length).toBe(4);
+    expect(countries.find("a").length).toBeLessThanOrEqual(4);
+    expect(countries.find("Z").length).toBeLessThanOrEqual(4);
 });
-// console.log(countries.find(" "));
+
+test("The search is case insensitive", () => {
+    expect(countries.find("a")).toEqual(countries.find("A"));
+    expect(countries.find("Z")).toEqual(countries.find("z"));
+});
+
+test("If there are no matching countries, an empty array is returned", () => {
+    expect(countries.find("asdaffa")).toEqual([]);
+    expect(countries.find("10")).toEqual([]);
+});
