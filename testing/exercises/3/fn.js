b92a3cb3a50dc95a30f01424942d5dbfac0d4c18
akram11 <akram.f11@gmail.com>
2020-09-02 18:52:15 +0200

    wrote a test for fn.js

--- testing/exercises/3/fn.js
+++ testing/exercises/3/fn.js
@@ -1,2 +1,15 @@
-module.exports = function fn() {
+module.exports = function fn(value) {
+    if (Array.isArray(value)) {
+        let result = [];
+        value.forEach((item) => {
+            result.push(fn(item));
+        });
+        return result;
+    } else if (typeof value === "string") {
+        return value.split("").reverse().join("");
+    } else {
+        return null;
+    }
 };
+
+// console.log(fn(["help", 213234, "helooeree"]));
